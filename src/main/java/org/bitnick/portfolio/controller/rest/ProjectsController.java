d45eab925092e0da90b2ec899f2e09e9b9b22d61
Nick <nstrydom@gmail.com>
2020-03-20 01:53:14 -0500

    Implemented ProjectsController.java

--- src/main/java/org/bitnick/portfolio/controller/rest/ProjectsController.java
+++ src/main/java/org/bitnick/portfolio/controller/rest/ProjectsController.java
@@ -1,48 +1,45 @@
 package org.bitnick.portfolio.controller.rest;
 
-import io.swagger.annotations.Api;
-import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponse;
-import io.swagger.annotations.ApiResponses;
+import org.bitnick.portfolio.model.Project;
 import org.bitnick.portfolio.model.User;
+import org.bitnick.portfolio.service.ProjectsService;
 import org.bitnick.portfolio.service.UserService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
-import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("/api/project")
 //@ApiIgnore
 public class ProjectsController {
-    private UserService userService;
+    private ProjectsService projectsService;
 
     private Logger LOG = LoggerFactory.getLogger(UsersController.class);
 
     @Autowired
-    public void setUserService(UserService userService) {
-        this.userService = userService;
+    public void setProjectsService(ProjectsService projectsService) {
+        this.projectsService = projectsService;
     }
 
     @RequestMapping(path = "{id}", method = RequestMethod.GET)
-    public User getUser(@PathVariable(name = "id") String id) {
-        return userService.getUser(id);
+    public Project getProject(@PathVariable(name = "id") String id) {
+        return projectsService.getProject(id);
     }
 
     @RequestMapping(method = RequestMethod.POST, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public User saveUser(@RequestBody User userToSave) {
-        return userService.saveUser(userToSave);
+    public Project saveProject(@RequestBody Project projectToSave) {
+        return projectsService.saveProject(projectToSave);
     }
 
     @RequestMapping(path = "{id}", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_VALUE)
-    public User updateUser(@RequestBody User userToUpdate, @PathVariable(name = "id") String id) {
-        return userService.updateUser(userToUpdate, id);
+    public Project updateProject(@RequestBody Project projectToUpdate, @PathVariable(name = "id") String id) {
+        return projectsService.updateProject(projectToUpdate, id);
     }
 
     @RequestMapping(path = "{id}", method = RequestMethod.DELETE)
     public void deleteUser(@PathVariable(name = "id") String id) {
-        userService.deleteUser(id);
+        projectsService.deleteProject(id);
     }
 }
