a48d1ab597bfcb11c691e2d776594f3f396e91ba
Nick <nstrydom@gmail.com>
2020-03-21 20:21:11 -0500

    Added shellSort() to AlgorithmService.java

--- src/main/java/org/bitnick/portfolio/service/AlgorithmService.java
+++ src/main/java/org/bitnick/portfolio/service/AlgorithmService.java
@@ -43,4 +43,23 @@ public class AlgorithmService {
 
         return bubbleSort(array, n - 1);
     }
+
+    public Integer[] shellSort(Integer[] array) {
+        int n = array.length;
+
+        for (int gap = n/2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i += 1) {
+                int temp = array[i];
+                int j;
+
+                for (j = i; j >= gap && array[j - gap] > temp; j -= gap) {
+                    array[j] = array[j - gap];
+                }
+
+                array[j] = temp;
+            }
+        }
+
+        return array;
+    }
 }
