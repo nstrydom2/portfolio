4dea28031fa3a522207f485b46d0cbc682c41d6a
Nick <nstrydom@gmail.com>
2020-03-24 11:57:09 -0500

    Reconfigured database

--- src/main/java/org/bitnick/portfolio/controller/rest/ProjectsController.java
+++ src/main/java/org/bitnick/portfolio/controller/rest/ProjectsController.java
@@ -7,6 +7,7 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
+import springfox.documentation.annotations.ApiIgnore;
 
 @RestController
 @RequestMapping("/api/project")
