6395d205f44145c36486889256718291c25d66c6
Nick <nstrydom@gmail.com>
2020-04-08 23:18:01 -0500

    Implement javadocs for UserService.java

--- src/main/java/org/bitnick/portfolio/service/UserService.java
+++ src/main/java/org/bitnick/portfolio/service/UserService.java
@@ -7,6 +7,10 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+/**
+ * This service implementation provides User entity input/output to
+ * and from the database utilizing the Jpa Repository UserRepository.java.
+ */
 @Service
 public class UserService {
     private UserRepository userRepository;
@@ -18,11 +22,21 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    /**
+     * Retrieves a mapped User entity by the given id.
+     * @param id UUID of the target User entity to be retrieved
+     * @return User retrieved via id
+     */
     public User getUser(String id) {
         LOG.info("Getting the product with the given id: " + id);
         return userRepository.getOne(id);
     }
 
+    /**
+     * Saves a User entity to the database.
+     * @param user User entity to be saved
+     * @return User, saved User entity
+     */
     public User saveUser(User user) {
         User userToSave = null;
 
@@ -39,6 +53,12 @@ public class UserService {
         }
     }
 
+    /**
+     * Updates a User to the database.
+     * @param user User entity with modified properties to be updated
+     * @param id UUID of target User to be updated
+     * @return User, updated User entity
+     */
     public User updateUser(User user, String id) {
         User userToUpdate = null;
 
@@ -53,10 +73,15 @@ public class UserService {
         } catch (Exception ex) {
             LOG.error("An error occurred during update of product" + ex.getMessage());
 
+            // return null if userToUpdate remains null
             return userToUpdate != null ? userToUpdate : new User();
         }
     }
 
+    /**
+     * Deletes a target User from the database.
+     * @param id UUID of target User to be deleted
+     */
     public void deleteUser(String id) {
         try {
             userRepository.deleteById(id);
