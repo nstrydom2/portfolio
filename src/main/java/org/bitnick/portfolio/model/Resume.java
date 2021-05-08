9332a34652b88d2e1e235d8efd58fcbfd428ef12
nstrydom2 <nstrydom@gmail.com>
2021-05-07 22:23:13 -0400

    Added @Table annotation to Resume.java

--- src/main/java/org/bitnick/portfolio/model/Resume.java
+++ src/main/java/org/bitnick/portfolio/model/Resume.java
@@ -2,13 +2,11 @@ package org.bitnick.portfolio.model;
 
 import org.hibernate.annotations.GenericGenerator;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import javax.persistence.*;
 import java.util.List;
 
 @Entity
+@Table(name = "resumes")
 public class Resume {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO, generator = "system-uuid")
